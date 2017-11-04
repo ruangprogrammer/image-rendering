36639c87fd95dda70c49185b9a7940c333640c80
Ruang Programmer <ruangprogramer@gmail.com>
2017-11-04 22:14:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html class='v2' dir='ltr' lang='en-US' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+<title>www.ruangprogrammer.com</title>
+<head>
+<link href='css.css' rel='stylesheet' type='text/css'/>
+</head>
+<body>
+<div class="original">
+  <h1>Original image size</h1> 
+  <img src="images/qrcode.png" alt="" />
+</div>
+
+<div class="resize">
+  <h2><code>image-rendering: auto</code></h2>
+  <img src="images/qrcode.png" alt="" />
+</div>
+
+<div class="resize">
+  <h2><code>image-rendering: pixelated</code></h2>
+  <img class="pixelated" src="images/qrcode.png" alt="" />
+</div>
+</body>
+</html>
